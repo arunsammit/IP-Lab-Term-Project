915345de6bbbf284364e935356ecef9b8e52cf1a
Sumegh-git <santurc2007@rediffmail.com>
2021-04-01 19:18:56 +0530

    small bug fix

--- Utility.py
+++ Utility.py
@@ -7,6 +7,8 @@ def getImages(imgsPath, masksPath):
         img = cv.imread(imgsPath + '/' + imgName)
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         mask = cv.imread(masksPath + '/' + imgName)
+        mask[mask<128] = 0
+        mask[mask>=128] = 255
         mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
         images.append((img, mask))
         names.append(imgName)
