f5f19884b38cd57366e75423e2e2a0b6448cb5c9
arunsammit <arunsammit@gmail.com>
2021-04-06 19:02:07 +0530

    modified \mu \sigma for better results, generated output images

--- generate_synthetic.py
+++ generate_synthetic.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 
 
-img = np.zeros((512, 512))
-mask = np.zeros((512, 512))
+img = np.zeros((512, 512),dtype=np.uint8)
+mask = np.zeros((512, 512),dtype=np.uint8)
 
 x1 = y1 = 128
 x2 = y2 = 128+256 # thresh
@@ -16,15 +16,10 @@ mask[x1:x2+1, y1:y2+1] = 255
 ht = mask.shape[0]
 wd = mask.shape[1]
 
-for id, (mu_back, mu_fore) in enumerate([(80,120), (130,140), (60,70)]):
-    for i in range(ht):
-        for j in range(wd):
-            if mask[i][j] == 0:
-                # back
-                img[i][j] = np.random.normal(mu_back, sigma_back)
-            else:
-                # fore
-                img[i][j] = np.random.normal(mu_fore, sigma_fore)
+for id, (mu_back, mu_fore) in enumerate([(112,144), (154,186), (70,102)]):
+    img = np.random.default_rng(seed = 99).normal(mu_back,sigma_back,size=(512,512))
+    np.putmask(img, mask == 255,np.random.default_rng(
+        seed=99).normal(mu_fore, sigma_fore, size=(x2-x1, y2-y1)))
     cv.imwrite("input_synthetic/image_" + str(id) + ".jpg", img)
     cv.imwrite("mask_synthetic/image_" + str(id) + ".jpg", mask)
 
